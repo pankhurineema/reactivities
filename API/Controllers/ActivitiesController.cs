7a6cceca728d9332de008728368d48c2055c5f79
yourusername <adarsh.tated@gmail.com>
2021-07-22 16:40:40 +0530

    test

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -13,7 +13,7 @@ namespace API.Controllers
         private readonly DataContext _context;
         public ActivitiesController(DataContext context)
         {
-            _context = context;
+            _context = context; 
         }
 
         [HttpGet]
