f48b2af6d6c37971a83d2e9013bcd4b4d60e0cd0
pankhurineema <pn6205@gmail.com>
2021-08-05 23:18:36 +0530

    End of section 7

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,103 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
-  const [activities,setActivities] = useState<Activity[]>([])
-  const [selectedActivity, setSelectedActivity]=useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [submitting, setSubmitting] = useState(false);
+  const {activityStore}=useStore();
 
   useEffect(()=>{
-   agent.Activities.List().then(response=>{
-     let activities: Activity[]=[];
-     response.forEach(activity => {
-       activities.push(activity);
-     })
-      setActivities(activities);
-      setLoading(false);
-    })
-  },[])
+  activityStore.loadActivities();
+  },[activityStore])
 
-  function handleSelectActivity(id:string)
-  {
-    setSelectedActivity(activities.find(x=>x.id===id))
-  }
 
-  function handleCancelActivity()
-  {
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string)
-  {
-    id ? handleSelectActivity(id) : handleCancelActivity();
-    setEditMode(true);
-  }
-
-  function handlFormClose(){
-    setEditMode(false);
-  }
-
-  function handleCreateOrEditActivity(activity:Activity)
-  {
-    setSubmitting(true);
-    if(activity.id)
-    {
-      agent.Activities.update(activity).then(()=>{
-        setActivities([...activities.filter(x=> x.id !== activity.id), activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }
-    else{
-      activity.id=uuid();
-      agent.Activities.create(activity).then(()=>{
-        setActivities([...activities,activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false); 
-      })
-    }
-  }
-
-  function handleDeleteActivity(id:string)
-  {
-    setSubmitting(true);agent.Activities.delete(id).then(()=>
-    {
-      setActivities([...activities.filter(x=> x.id !== id)]);
-    })
-    
-  }
-
-  if(loading) return <LoadingComponent content='Loading app' />
+  if(activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
   return (
     <>
-      <NavBar openForm={handleFormOpen}/>
+      <NavBar/>
       <Container style={{marginTop:'7em'}}>
-       <ActivityDashboard 
-        activities={activities}
-        selectedActivity={selectedActivity}
-        selectActivity={handleSelectActivity}
-        cancleSelectActivity={handleCancelActivity}
-        editMode={editMode}
-        openForm={handleFormOpen}
-        closeForm={handlFormClose}
-        createOrEdit={handleCreateOrEditActivity}
-        deleteActivity={handleDeleteActivity}
-        submitting={submitting}
-       />
+       <ActivityDashboard />
       </Container>
       
       </>
   );
 }
 
-export default App;
+export default observer(App) ;
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,18 +1,12 @@
-import React from "react";
+import { observer } from "mobx-react-lite";
 import { SyntheticEvent } from "react";
 import { useState } from "react";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
 
-interface Props{
-    activities: Activity[];
-    selectActivity:(id:string) => void;
-    deleteActivity:(id:string) => void;
-    submitting:boolean;
-}
-
-export default function ActivityList({activities,selectActivity,deleteActivity,submitting}:Props){
-
+export default observer(function ActivityList(){
+    const {activityStore}=useStore(); 
+    const {deleteActivity,activitiesByDate,loading} =activityStore;
     const [target,setTarget] = useState('');
 
     function handleActivityDelete(e:SyntheticEvent<HTMLButtonElement>, id:string)
@@ -21,10 +15,12 @@ export default function ActivityList({activities,selectActivity,deleteActivity,s
         deleteActivity(id);
     }
     
+    
+
     return(
         <Segment>
             <Item.Group divided>
-                { activities.map(activity =>(
+                { activitiesByDate.map(activity =>(
                     <Item key={activity.id}>
                          <Item.Content>
                              <Item.Header as='a'>{activity.title}</Item.Header>
@@ -34,8 +30,8 @@ export default function ActivityList({activities,selectActivity,deleteActivity,s
                                  <div>{activity.city},{activity.venue}</div>
                              </Item.Description>
                              <Item.Extra>
-                                 <Button onClick={()=>selectActivity(activity.id)} floated='right' content='View' color='blue'/>
-                                 <Button name={activity.id} loading={submitting && target === activity.id} onClick={(e)=>handleActivityDelete(e,activity.id)} floated='right' content='Delete' color='red'/>
+                                 <Button onClick={()=>activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue'/>
+                                 <Button name={activity.id} loading={loading && target === activity.id} onClick={(e)=>handleActivityDelete(e,activity.id)} floated='right' content='Delete' color='red'/>
                                  <Label basic content={activity.category}/>
                              </Item.Extra>
                          </Item.Content>
@@ -44,4 +40,4 @@ export default function ActivityList({activities,selectActivity,deleteActivity,s
             </Item.Group>
         </Segment>
     )
-}
+})
